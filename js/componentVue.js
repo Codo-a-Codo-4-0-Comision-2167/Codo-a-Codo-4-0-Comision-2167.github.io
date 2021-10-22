39880253e62ffaeff11aff9a7669b2b8cd4d9af9
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-10-22 20:31:10 -0300

    last commit of the day

--- js/componentVue.js
+++ js/componentVue.js
@@ -1,10 +1,39 @@
+Vue.component('controls', {
+    template: "<h1>Spotify - Tema musical </h1>"
+})
+
+let myJSONString = '{"message":"Hello World","otraData":"Bla bla","control_enabled":true,"counter":0,"current_time":0,"total_time":0}'
+
+let myJSONObject = JSON.parse(myJSONString)
+
 const app = new Vue({
     el: '#vueJS',
-    data: {
-        message: "Hello World", 
-        otraData: "Bla bla",
-        control_enabled: true,
-        counter: 0
+    data: myJSONObject,
+    methods: {
+        incrementCounter() {
+            this.counter += 1;
+        },
+        decreaseCounter() {
+            if (this.counter > 0) {
+                this.counter -= 1;
+            }
+        }
+    },
+    computed: {
+        remainingTime() {
+            return this.total_time - this.current_time;
+        }
     }
- });
+});
 
+app.total_time = 100; // El tiempo de la musica es 100 segundos
+
+
+fetch("https://randomuser.me/api")
+    .then(response => response.json())
+    .then(data => { 
+        console.log(data)
+
+        app.message = data.results[0].picture.medium;
+
+    });
